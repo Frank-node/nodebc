f6074313c82845f51b520ff29375e38a949e7365
Frank <gufang73@hotmail.com>
2020-01-22 16:32:09 -0800

    Added js exercises 8-9 for const in js folder

---
+++ js/index8.js
@@ -0,0 +1,11 @@
+const firstName = 'Frank';
+const lastName = 'Gu';
+const age = 47;
+const address = 'Richmond';
+const birthDate = '04/08/1973';
+
+console.log('Firstname: ', firstName);
+console.log('Lastname: ', lastName);
+console.log('Age: ', age);
+console.log('Address: ', address);
+console.log('Birthdate: ', birthDate);
