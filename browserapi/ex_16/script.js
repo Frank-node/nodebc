746845fd3b1cdaa2cc2f52b47687924655c68742
Frank <gufang73@hotmail.com>
2020-02-14 11:19:47 -0800

    Adding exercises about Selectors, Element Attributes

---
+++ browserapi/ex_16/script.js
@@ -0,0 +1,8 @@
+
+    const linkElement = document.querySelector("a");
+    console.log(linkElement.attributes);
+    const hrefAttr = linkElement.getAttribute('href');
+    console.log(hrefAttr);
+    const classAttr = linkElement.className
+    console.log(classAttr);
+  
